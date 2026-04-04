60c02a1f4b75740f3f9c17542a10dc9315217d53
Prash-Ant-Magician <prashantkumar843319@gmail.com>
2026-04-04 09:19:42 +0530

    Day_0 Practice

--- daily/day_0.py
+++ daily/day_0.py
@@ -5,3 +5,8 @@ print("___"*40)
 print("NETWORKING LAB")
 print("SETUP VERIFICATION DAY_0")
 print("___"*40)
+
+
+print(f"\n PYTHON VERSION: {sys.version}")
+print(f"PLATFORM: {platform.version()} {platform.release}")
+print(f"ARCHITECTURE: {platform.machine()}")
